19f7e7a0bf3e835917c674cc7d7615f211fbc4b2
Io-Saito <ioio3110@gmail.com>
2021-03-02 01:06:52 +0900

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  siteMetadata: {
+    title: "gatsby-site2",
+  },
+  plugins: [
+    "gatsby-plugin-mdx",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "pages",
+        path: "./src/pages/",
+      },
+      __key: "pages",
+    },
+  ],
+};
